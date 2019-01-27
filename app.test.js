1c5bf914549e1254f021124418818a32df035cb8
Oleg Zarevych <Oleg.Zarevych@gmail.com>
2019-01-28 00:57:40 +0200

    add JEST test with puppeter

--- app.test.js
+++ app.test.js
@@ -1,7 +1,16 @@
 var puppeteer = require("puppeteer")
 
-describe('on load', ()=> {
-    test('header loads', async => {
+describe('on page load', () => {
+    test('text exist', async () => {
+        let browser = await puppeteer.launch()
+        let page = await browser.newPage()
 
-    })
+        await page.goto('http://duckduckgo.com/')
+
+        const msg = await page.$eval('div.tag-home__item', e => e.innerHTML)
+
+        expect(msg).toContain("The search engine that doesn't track you.");
+
+        browser.close()
+    }, 16000)
 })

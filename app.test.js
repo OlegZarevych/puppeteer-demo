919325cc8a1e7eb0fc53ced16c5c50fed967587f
Oleg Zarevych <Oleg.Zarevych@gmail.com>
2019-01-28 01:03:45 +0200

    move common logic to before\after conditions

--- app.test.js
+++ app.test.js
@@ -1,16 +1,24 @@
 var puppeteer = require("puppeteer")
 
+let browser
+let page
+
+beforeAll(async () => {
+    browser = await puppeteer.launch()
+    page = await browser.newPage()
+
+    await page.goto('http://duckduckgo.com/')
+})
+
+afterAll(() =>{
+    browser.close();
+})
+
 describe('on page load', () => {
     test('text exist', async () => {
-        let browser = await puppeteer.launch()
-        let page = await browser.newPage()
-
-        await page.goto('http://duckduckgo.com/')
 
         const msg = await page.$eval('div.tag-home__item', e => e.innerHTML)
 
         expect(msg).toContain("The search engine that doesn't track you.");
-
-        browser.close()
     }, 16000)
 })
